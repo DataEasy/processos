8ce391ae3f280d0a237ed42d3d85a9c75da12670
Luiz Gonzaga dos Santos Filho <lfilho@gmail.com>
2014-12-01 11:05:56 -0200

    Limpando diretório remoto antes de enviar arquivos novos

--- publish.sh
+++ publish.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env expect -f
+spawn ssh -o "StrictHostKeyChecking no" processos@processos.dataeasy.com.br rm -rf *
+expect "password:"
+send "processos\r"
+
 spawn scp -o "StrictHostKeyChecking no" -r . processos@processos.dataeasy.com.br:.
 expect "password:"
 send "processos\r"
+
 expect eof
