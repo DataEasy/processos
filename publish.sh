9f0905d00c31c0bd901a54ed9dcf0a181c1c6998
Luiz Gonzaga dos Santos Filho <lfilho@gmail.com>
2014-12-01 17:27:08 -0200

    Usando chave jenkins alternadamente

--- publish.sh
+++ publish.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env sh
-ssh -o "StrictHostKeyChecking no" processos@processos.dataeasy.com.br rm -rf *
-scp -C -o "StrictHostKeyChecking no" -r . processos@processos.dataeasy.com.br:.
+OPTION=$JENKINS_SSH_KEY_FILE
+PARAM="-i $JENKINS_SSH_KEY_FILE"
+
+if [ "${OPTION+x}" = "x" ]; then
+    PARAM=""
+fi
+
+ssh -o "StrictHostKeyChecking no" $PARAM processos@processos.dataeasy.com.br rm -rf *
+scp -C -o "StrictHostKeyChecking no" $PARAM -r . processos@processos.dataeasy.com.br:.
 
